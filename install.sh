e108ca74f47e72a85dfc24c57d39fb6f554e095f
jKey Lu <jkeylu@gmail.com>
2016-05-07 17:43:54 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,16 @@
-#!/bin/sh
+#!/usr/bin/env bash
 #
 # name    : vimx
 # author  : jKey Lu <jkeylu@gmail.com>
 # license : GPL
 #
 
-ln -s ~/.vim/vimrc ~/.vimrc
-mkdir ~/.vim/bundle
-git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+[[ -f ~/.vimrc ]] && mv ~/.vimrc ~/.vimrc.bak
+[[ -L ~/.vimrc ]] || ln -s ~/.vim/vimrc ~/.vimrc
+
+[[ -L ~/.config/nvim ]] || ln -s ~/.vim ~/.config/nvim
+
+[[ -d ~/.vim/bundle ]] || mkdir ~/.vim/bundle
+[[ -d ~/.vim/bundle/neobundle.vim ]] || git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+
 vim +NeoBundleInstall +qall
